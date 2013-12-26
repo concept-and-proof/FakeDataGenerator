628566e06e60f214df6bb5d1dfba2601a1613591
CrazyCoder <true.isnt.false@gmail.com>
2013-12-26 22:23:59 +0700

    many changes

---
+++ app.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is a part of FakeDataGenerator
+ * Brought to you by Concept&Proof 
+ */
+
+error_reporting (-1);
+ini_set ('display_errors', true);
+
+date_default_timezone_set ('UTC');
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
