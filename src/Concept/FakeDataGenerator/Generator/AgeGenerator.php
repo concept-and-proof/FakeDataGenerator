d89a9a6e128b390dc211e2af78594cb8c751f4f8
CrazyCoder <true.isnt.false@gmail.com>
2013-12-26 21:47:42 +0700

    added AgeGenerator - simple example

---
+++ src/Concept/FakeDataGenerator/Generator/AgeGenerator.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is a part of FakeDataGenerator
+ * Brought to you by Concept&Proof 
+ */
+
+namespace Concept\FakeDataGenerator\Generator;
+
+class AgeGenerator implements GeneratorInterface
+{
+    public function generate ( $additionalInfo = [] )
+    {
+        $minAge = 1; 
+        $maxAge = 100;
+        
+        if ( array_key_exists ('minAge', $additionalInfo) )
+        {
+            $minAge = (int)$additionalInfo ['minAge'];
+        }
+        
+        if ( array_key_exists ('maxAge', $additionalInfo) )
+        {
+            $maxAge = (int)$additionalInfo ['maxInfo'];
+        }
+        
+        return rand ($minAge, $maxAge);
+    }
+}
+
