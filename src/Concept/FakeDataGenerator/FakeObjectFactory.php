b6ff6cf38f5e6afd0d1f086ed74b858370299b2d
CrazyCoder <true.isnt.false@gmail.com>
2013-12-27 15:35:03 +0700

    fakeobjectfactory

--- src/Concept/FakeDataGenerator/FakeObjectFactory.php
+++ src/Concept/FakeDataGenerator/FakeObjectFactory.php
@@ -9,5 +9,43 @@ namespace Concept\FakeDataGenerator;
 
 class FakeObjectFactory
 {
+    protected $_fakeObject = null; 
+    protected $_amount = null;
     
+    public static function create ()
+    {        
+        return new static;
+    }    
+    
+    public function setFakeObject (FakeEntityInterface $fakeObject)
+    {
+        $this->_fakeObject = $fakeObject;
+    }
+    
+    public function setAmount ($amount)
+    {
+        $amount = (int)$amount;
+        
+        if (0 >= $amount)
+        {
+            throw new \InvalidArgumentException ();
+        }
+        
+        $this->_amount = $amount;
+    }
+    
+    public function produce ()
+    {        
+        if (is_null ($this->_amount) || is_null ($this->_fakeObject))
+        {
+            throw new \BadMethodCallException ();
+        }
+        
+        for ( $i = 0; $i < $this->_amount; $i++ )
+        {
+            $fakeObject = clone $this->_fakeObject;
+            $fakeObject->fillInData ();
+            yield ($fakeObject); // new php 5.5 feature in action!
+        }
+    }
 }
